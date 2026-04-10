faa3a4e6d97591eb55f8b061de17102f335b4ede
ilyesbeny <iby.ilyes@gmail.com>
2026-04-10 09:48:34 +0200

    Add union query for September 2022 voyages

--- models/00_staging/voyages.sql
+++ models/00_staging/voyages.sql
@@ -1,3 +1,5 @@
-select
-    *
-from {{ source("dbt_intro", "voyage_08_2022") }}
+select * from {{ source("dbt_intro", "voyage_08_2022") }}
+
+union all
+
+select * from {{ source("dbt_intro", "voyage_09_2022") }}
